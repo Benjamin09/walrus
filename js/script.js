41be3b43f20f077f5821035295e9b96cc30f90c6
Benjamin Heflin and Charles Ewel <benjamin@>
2016-05-31 11:38:38 -0700

    add a second page with other silly animals

--- js/script.js
+++ js/script.js
@@ -4,3 +4,17 @@ $(document).ready(function() {
     $("#initially-showing").slideToggle();
   });
 });
+
+$(document).ready(function() {
+  $(".clickable-tarsier").click(function() {
+    $("#initially-hidden-tarsier").slideToggle();
+    $("#initially-showing-tarsier").slideToggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable-sloth").click(function() {
+    $("#initially-hidden-sloth").slideToggle();
+    $("#initially-showing-sloth").slideToggle();
+  });
+});
